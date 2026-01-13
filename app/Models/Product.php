83b957270a098536538f26c77f2ec1fe3707a220
hobbit254 <kevinmkenyainfosec@gmail.com>
2026-01-13 12:43:33 +0300

    Update Product.php

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -74,7 +74,9 @@ class Product extends Model
 
     public function images()
     {
-        return $this->hasMany(ProductImages::class, 'product_id');
+        return $this->hasMany(ProductImages::class, 'product_id')
+            ->orderByDesc('primary_image')
+            ->orderBy('id');
     }
 
 }
