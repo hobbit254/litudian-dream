9c8365792644c6ddbed9ebf058bccae198360c60
hobbit254 <kevinmkenyainfosec@gmail.com>
2026-01-13 13:07:36 +0300

    Update ProductController.php

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -236,6 +236,7 @@ class ProductController extends Controller
     {
         $product = Product::join('categories', 'categories.id', '=', 'products.category_id')
             ->where('products.uuid', $id)
+            ->with('product_images')
             ->first();
         if (!$product) {
             return ResponseHelper::error([], 'Product not found.', 404);
